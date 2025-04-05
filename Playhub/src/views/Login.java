370ec94144961b04dad249b278df90860ebe5997
Tonii18 <ajsanrosal@gmail.com>
2025-04-05 18:50:09 +0200

    Commit #1

--- Playhub/src/views/Login.java
+++ Playhub/src/views/Login.java
@@ -34,6 +34,7 @@ public class Login extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setLocationRelativeTo(null);
 
 		setContentPane(contentPane);
 	}
